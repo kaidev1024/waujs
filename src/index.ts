8c668e3d12825698ac12e33fd8ff45697c9955f9
Xiaokai Li <xiaokai.li-1@hotmail.com>
2026-01-01 19:51:19 -0800

    minor fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-export {
-  readBlob,
-  getBlobByS3Url,
-  getDataUrlByS3Url,
-} from './FileReader';
+export * from './fileReader';
 export { twConcat, twcn } from './tailwind';
 export { ZeroFunc, BoolFunc } from './functions';
 export type { ZeroFuncType, BoolFuncType } from './functions';
